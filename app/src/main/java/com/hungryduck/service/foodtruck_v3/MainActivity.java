a6e98ea50784db6e9d71e2d28d2eec71e189fcb9
jason <jason@iamcompany.net>
2018-01-29 01:56:21 +0900

    init

---
+++ app/src/main/java/com/hungryduck/service/foodtruck_v3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hungryduck.service.foodtruck_v3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
